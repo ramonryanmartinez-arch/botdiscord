0de1fbb1484819533cb3290d1d8e79a5997306b1
Ramon <ramonryanmartinez@gmail.com>
2026-05-16 12:02:17 -0300

    add mutacoes pele file

---
+++ data/mutacoesPele.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bloodroot: 2,
+  candy: 3,
+  lava: 6,
+  galaxy: 7,
+  radioativa: 7.5,
+  "ying yang": 8.5,
+  cursed: 9.5,
+  divina: 10,
+  cyber: 11,
+  gold: 1.25,
+  diamante: 1.75,
+  rainbow: 10
+}; 
